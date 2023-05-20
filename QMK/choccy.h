dd41d9f627bbe07fa26e6ab8662bd573b1fcfdf6
gn3rps <125663243+gn3rps@users.noreply.github.com>
2023-05-19 22:40:35 -0500

    Update choccy.h

--- QMK/choccy.h
+++ QMK/choccy.h
@@ -22,8 +22,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "quantum.h"
 
-//#define XXX KC_NO
-
 #define LAYOUT( \
 	LA1, LA2, LA3, LA4, LA5, LA6,				RA1, RA2, RA3, RA4, RA5, RA6, \
 	LB1, LB2, LB3, LB4, LB5, LB6,				RB1, RB2, RB3, RB4, RB5, RB6, \
